5dc4d8ee9a7ad920d0919548acb6d77fea201837
younes <younesharke@gmail.comgit config --global user.email younesharke@gmail.comgit config --global user.email younesharke@gmail.com>
2019-12-04 01:51:27 +0100

    add jauge cpt

--- gulpfile.js
+++ gulpfile.js
@@ -20,109 +20,112 @@ var replace = require("gulp-replace");
 //@@STEP--01 : source/dist const path define
 // source File paths
 const source = {
-  scssPath: "app/scss/**/*.scss",
-  jsPath: "app/js/**/*.js", // "app/js/**/*.js" |  "app/js/script.js"
-  imgPath: "app/img/**/*.*",
-  fontPath: "app/fonts/**/*.*",
-  tplPath: "app/twig/**/*.*",
-  dataPath: "app/data/**/*.*"
+    scssPath: "app/scss/**/*.scss",
+    jsPath: "app/js/**/*.js", // "app/js/**/*.js" |  "app/js/script.js"
+    imgPath: "app/img/**/*.*",
+    fontPath: "app/fonts/**/*.*",
+    tplPath: "app/twig/**/*.*",
+    dataPath: "app/data/**/*.*"
 };
 
 // distination File paths
 const dist = {
-  scssPath: "dist",
-  jsPath: "dist",
-  imgPath: "dist/img",
-  fontPath: "dist/fonts",
-  tplPath: "dist",
-  dataPath: "dist/data",
-  ServerEnterLoad: "dist" // entry point for loading/reloading browser
+    scssPath: "dist",
+    jsPath: "dist",
+    imgPath: "dist/img",
+    fontPath: "dist/fonts",
+    tplPath: "dist",
+    dataPath: "dist/data",
+    ServerEnterLoad: "dist" // entry point for loading/reloading browser
 };
 
 // local Server and reload after updating
 function reload(done) {
-  server.reload();
-  done();
+    server.reload();
+    done();
 }
+
 function serve(done) {
-  server.init({
-    server: {
-      baseDir: dist.ServerEnterLoad
-    }
-  });
-  done();
+    server.init({
+        server: {
+            baseDir: dist.ServerEnterLoad
+        }
+    });
+    done();
 }
 
 //@@STEP--02 : function task define
 
 // Sass task: compiles the style.scss file into style.css
 function scssTask() {
-  return src(source.scssPath)
-    .pipe(sourcemaps.init()) // initialize sourcemaps first
-    .pipe(sass()) // compile SCSS to CSS
-    .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
-    .pipe(sourcemaps.write(".")) // write sourcemaps file in current directory
-    .pipe(dest(dist.scssPath)); // put final CSS in dist folder
+    return src(source.scssPath)
+        .pipe(sourcemaps.init()) // initialize sourcemaps first
+        .pipe(sass()) // compile SCSS to CSS
+        .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
+        .pipe(sourcemaps.write(".")) // write sourcemaps file in current directory
+        .pipe(dest(dist.scssPath)); // put final CSS in dist folder
 }
 
 // images task : compile img task
 function imgTask() {
-  return src(source.imgPath)
-    .pipe(imagemin({ verbose: true })) // verbose: true > display in console image mimification journal
-    .pipe(dest(dist.imgPath));
+    return src(source.imgPath)
+        .pipe(imagemin({ verbose: true })) // verbose: true > display in console image mimification journal
+        .pipe(dest(dist.imgPath));
 }
 
 // fonts task : compile fonts task
 function fontTask() {
-  return src(source.fontPath).pipe(dest(dist.fontPath));
+    return src(source.fontPath).pipe(dest(dist.fontPath));
 }
 
 // fonts task : compile fonts task
 function dataTask() {
-  return src(source.dataPath).pipe(dest(dist.dataPath));
+    return src(source.dataPath).pipe(dest(dist.dataPath));
 }
 // JS task: concatenates and uglifies JS files to script.js
 function jsTask() {
-  return src([
-    "node_modules/jquery/dist/jquery.js",
-    "node_modules/flatpickr/dist/flatpickr.js", // lib pour les calendrier
-    source.jsPath
-  ])
-    .pipe(concat("all.js"))
-    .pipe(uglify())
-    .pipe(dest(dist.jsPath));
+    return src([
+            "node_modules/jquery/dist/jquery.js",
+            "node_modules/flatpickr/dist/flatpickr.js", // lib pour les calendrier
+            source.jsPath
+        ])
+        .pipe(concat("all.js"))
+        .pipe(uglify())
+        .pipe(dest(dist.jsPath));
 }
 
 // Templating task: TWIG
 function tplTask() {
-  console.log("tplTask : OK !!");
-  return src([source.tplPath]) // ["app/twig/5-page/*.*", "app/twig/**/*.*"]  ||  source.tplPath
-    .pipe(
-      twig({
-        data: {
-          // here define a global variable for all site
-          siteName: "Kit-starter for web template 201910 v 1.0",
-          // config specifique au projet: c'est des variable global dans le twig
-          TplInputCheckbox: "includes/forms/input-checkbox.twig",
-          TplInputRadio: "includes/forms/input-radio.twig",
-          TplEventLineStay: "includes/event-line-stay.twig",
-          TplEventLineDays: "includes/event-line-days.twig",
-          TplMainTopBtns: "includes/main-top-btns.twig"
-        },
-        errorLogToConsole: true,
-        cache: false
-        // cacheBustTask: false
-      })
-    )
-    .pipe(dest(dist.tplPath));
+    console.log("tplTask : OK !!");
+    return src([source.tplPath]) // ["app/twig/5-page/*.*", "app/twig/**/*.*"]  ||  source.tplPath
+        .pipe(
+            twig({
+                data: {
+                    // here define a global variable for all site
+                    siteName: "Kit-starter for web template 201910 v 1.0",
+                    // config specifique au projet: c'est des variable global dans le twig
+                    TplInputCheckbox: "includes/forms/input-checkbox.twig",
+                    TplInputRadio: "includes/forms/input-radio.twig",
+                    TplEventLineStay: "includes/event-line-stay.twig",
+                    TplEventLineDays: "includes/event-line-days.twig",
+                    TplMainTopBtns: "includes/main-top-btns.twig",
+                    TplCptJauge: "includes/components/jauge/jauge.twig"
+                },
+                errorLogToConsole: true,
+                cache: false
+                    // cacheBustTask: false
+            })
+        )
+        .pipe(dest(dist.tplPath));
 }
 
 // Cachebust : add param version to called files ( js ,css ..) in html page, to remove persistance browser cache
 var cbString = new Date().getTime();
+
 function cacheBustTask() {
-  return src([dist.ServerEnterLoad + "/*.html"])
-    .pipe(replace(/cb=\d+/g, "cb=" + cbString))
-    .pipe(dest(dist.ServerEnterLoad));
+    return src([dist.ServerEnterLoad + "/*.html"])
+        .pipe(replace(/cb=\d+/g, "cb=" + cbString))
+        .pipe(dest(dist.ServerEnterLoad));
 }
 
 //@@STEP--03 : watch path/task implement > watch([PATH1, PAHT2,...], parallel(TASK1,TASK2,...));
@@ -130,18 +133,18 @@ function cacheBustTask() {
 // Watch task: watch SCSS and JS files for changes
 // If any change, run scss and js tasks simultaneously
 function watchTask() {
-  // livereload.listen();
-  watch(
-    [
-      source.scssPath,
-      source.jsPath,
-      source.imgPath,
-      source.fontPath,
-      source.dataPath,
-      source.tplPath
-    ],
-    parallel(scssTask, jsTask, imgTask, fontTask, dataTask, tplTask, reload)
-  );
+    // livereload.listen();
+    watch(
+        [
+            source.scssPath,
+            source.jsPath,
+            source.imgPath,
+            source.fontPath,
+            source.dataPath,
+            source.tplPath
+        ],
+        parallel(scssTask, jsTask, imgTask, fontTask, dataTask, tplTask, reload)
+    );
 }
 
 //@@STEP--04 : default gulb launch serices (cmd: gulp) > implementing tasks
@@ -149,7 +152,7 @@ function watchTask() {
 // Runs the scss and js tasks simultaneously
 // then runs cacheBust, then watch task
 exports.default = series(
-  parallel(serve, jsTask, imgTask, fontTask, dataTask, tplTask, scssTask),
-  cacheBustTask,
-  watchTask
+    parallel(serve, jsTask, imgTask, fontTask, dataTask, tplTask, scssTask),
+    cacheBustTask,
+    watchTask
 );
